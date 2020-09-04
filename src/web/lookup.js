johnfactotum/foliate

commit 72d5c701df3b3ad355581ef31e4a54c4af19cca1

Use iso-codes to display language names
Also use it to convert alpha-3 language codes to alpha-2, for Wikipedia
and Wiktionary lookup.

For info on the iso-codes project, see
https://salsa.debian.org/iso-codes-team/iso-codes

src/web/lookup.js
@@ -14,7 +14,6 @@
  */
 
 const wiktionary = (word, language = 'en') => {
-    language = language.slice(0, 2).toLowerCase()
     const baseURL = 'https://en.wiktionary.org/'
     fetch(`https://en.wiktionary.org/api/rest_v1/page/definition/${word}`)
         .then(res => res.ok ? res.json() : Promise.reject(new Error()))

src/lookup.js
@@ -14,7 +14,7 @@
  */
 
 const { GObject, GLib, Gtk, Gdk, WebKit2 } = imports.gi
-const { execCommand, user_agent } = imports.utils
+const { execCommand, user_agent, getLanguageDisplayName, getAlpha2 } = imports.utils
 
 const lookup = (script, againScript) => new Promise((resolve, reject) => {
     const webView = new WebKit2.WebView({
@@ -85,7 +85,7 @@ var WikipediaBox = GObject.registerClass({
     InternalChildren: ['wikiStack', 'wikiContent', 'wikiErrorLabel', 'wikiButton']
 }, class WikipediaBox extends Gtk.Box {
     lookup(text, language) {
-        language = language.slice(0, 2).toLowerCase()
+        language = getAlpha2(language)
         this._wikiStack.visible_child_name = 'loading'
         this._wikiContent.foreach(child => this._wikiContent.remove(child))
         wikipedia(text, language)
@@ -352,6 +352,7 @@ var DictionaryBox = GObject.registerClass({
         this._dictEntry.connect('activate', () => this._lookupFromEntry())
     }
     lookup(text, language) {
+        language = getAlpha2(language)
         this.language = language
         this._dictEntry.text = text
         this._lookupFromEntry()
@@ -416,14 +417,38 @@ const translate = (word, language = 'en') => lookup(
     return box
 })
 
+/*
+List of languages supported by Google Translate
+Generated by running the following on https://cloud.google.com/translate/docs/languages
+
+```
+[...document.querySelectorAll('tbody tr')].map(tr => `'${tr.querySelector('code').innerText}'`).join(',')
+```
+*/
+const translationLangs = [
+    'af','sq','am','ar','hy','az','eu','be','bn','bs','bg','ca','ceb',
+    'zh-CN','zh-TW','co','hr','cs','da','nl','en','eo','et','fi','fr',
+    'fy','gl','ka','de','el','gu','ht','ha','haw','he','hi','hmn','hu',
+    'is','ig','id','ga','it','ja','jv','kn','kk','km','rw','ko','ku',
+    'ky','lo','la','lv','lt','lb','mk','mg','ms','ml','mt','mi','mr',
+    'mn','my','ne','no','ny','or','ps','fa','pl','pt','pa','ro','ru',
+    'sm','gd','sr','st','sn','sd','si','sk','sl','so','es','su','sw',
+    'sv','tl','tg','ta','tt','te','th','tr','tk','uk','ur','ug','uz',
+    'vi','cy','xh','yi','yo','zu'
+]
+
 var TranslationBox = GObject.registerClass({
     GTypeName: 'FoliateTranslationBox',
     Template: 'resource:///com/github/johnfactotum/Foliate/ui/translationBox.ui',
     Children: ['langCombo'],
-    InternalChildren: ['translationStack', 'translationContent']
+    InternalChildren: ['langStore', 'translationStack', 'translationContent']
 }, class TranslationBox extends Gtk.Box {
     _init(params, lang = 'en') {
         super._init(params)
+        translationLangs.forEach(lang => {
+            this._langStore.set(this._langStore.append(),
+                [0, 1], [lang, getLanguageDisplayName(lang)])
+        })
         this.langCombo.active_id = lang
         this.langCombo.connect('changed', () => this._langChanged())
     }

src/com.github.johnfactotum.Foliate.src.gresource.xml
@@ -16,5 +16,6 @@
     <file>catalogs.js</file>
     <file>epubcfi.js</file>
     <file>handy.js</file>
+    <file>isoCodes.js</file>
   </gresource>
 </gresources>

src/isoCodes.js.in
@@ -1,0 +1,12 @@
+const { GLib } = imports.gi
+const isoCodesPrefix = '@ISO_CODES_PREFIX@'
+
+var iso_639_2_path = GLib.build_filenamev([
+    isoCodesPrefix,
+    '/share/iso-codes/json/iso_639-2.json'
+])
+
+var iso_3166_1_path = GLib.build_filenamev([
+    isoCodesPrefix,
+    '/share/iso-codes/json/iso_3166-1.json'
+])

src/meson.build
@@ -1,11 +1,28 @@
 pkgdatadir = join_paths(get_option('datadir'), meson.project_name())
 gnome = import('gnome')
 
+iso_codes = dependency('iso-codes', version: '>= 3.67', required: false)
+if iso_codes.found()
+  iso_codes_prefix = iso_codes.get_pkgconfig_variable('prefix')
+else
+  iso_codes_prefix = '/usr'
+endif
+
+iso_codes_conf = configuration_data()
+iso_codes_conf.set('ISO_CODES_PREFIX', iso_codes_prefix)
+
+iso_codes_js = configure_file(
+  input: 'isoCodes.js.in',
+  output: 'isoCodes.js',
+  configuration: iso_codes_conf,
+)
+
 src_res = gnome.compile_resources('com.github.johnfactotum.Foliate.src',
   'com.github.johnfactotum.Foliate.src.gresource.xml',
   gresource_bundle: true,
   install: true,
   install_dir: pkgdatadir,
+  dependencies: [iso_codes_js],
 )
 
 data_res = gnome.compile_resources('com.github.johnfactotum.Foliate.data',

src/properties.js
@@ -14,7 +14,7 @@
  */
 
 const { GObject, Gtk, Gio, Gdk } = imports.gi
-const { scalePixbuf, makeLinksButton } = imports.utils
+const { scalePixbuf, makeLinksButton, getLanguageDisplayName } = imports.utils
 
 const PropertyBox = GObject.registerClass({
     GTypeName: 'FoliatePropertyBox',
@@ -87,10 +87,13 @@ var PropertiesBox = GObject.registerClass({
             property_name: _('Modified Date'),
             property_value: modified_date
         }), false, true, 0)
-        if (language) this._propertiesBox.pack_start(new PropertyBox({
-            property_name: _('Language'),
-            property_value: language
-        }), false, true, 0)
+        if (language) {
+            const name = getLanguageDisplayName(language)
+            if (name) this._propertiesBox.pack_start(new PropertyBox({
+                property_name: _('Language'),
+                property_value: name
+            }), false, true, 0)
+        }
         if (extent) this._propertiesBox.pack_start(new PropertyBox({
             property_name: _('File Size'),
             property_value: extent

src/ui/translationBox.ui
@@ -102,442 +102,10 @@
       </object>
     </child>
   </template>
-<!--
-List of languages supported by Google Translate
-Generated by running the following on https://cloud.google.com/translate/docs/languages
-
-```javascript
-[...document.querySelectorAll('tbody tr')]
-.map(tr => `
-      <row>
-        <col id="0">${tr.querySelector('code').innerText}</col>
-        <col id="1" translatable="yes">${tr.querySelector('td').innerText}</col>
-      </row>`)
-.join('')
-```
--->
   <object class="GtkListStore" id="langStore">
     <columns>
       <column type="gchararray"/>
       <column type="gchararray"/>
     </columns>
-    <data>
-      <row>
-        <col id="0">af</col>
-        <col id="1" translatable="yes">Afrikaans</col>
-      </row>
-      <row>
-        <col id="0">sq</col>
-        <col id="1" translatable="yes">Albanian</col>
-      </row>
-      <row>
-        <col id="0">am</col>
-        <col id="1" translatable="yes">Amharic</col>
-      </row>
-      <row>
-        <col id="0">ar</col>
-        <col id="1" translatable="yes">Arabic</col>
-      </row>
-      <row>
-        <col id="0">hy</col>
-        <col id="1" translatable="yes">Armenian</col>
-      </row>
-      <row>
-        <col id="0">az</col>
-        <col id="1" translatable="yes">Azerbaijani</col>
-      </row>
-      <row>
-        <col id="0">eu</col>
-        <col id="1" translatable="yes">Basque</col>
-      </row>
-      <row>
-        <col id="0">be</col>
-        <col id="1" translatable="yes">Belarusian</col>
-      </row>
-      <row>
-        <col id="0">bn</col>
-        <col id="1" translatable="yes">Bengali</col>
-      </row>
-      <row>
-        <col id="0">bs</col>
-        <col id="1" translatable="yes">Bosnian</col>
-      </row>
-      <row>
-        <col id="0">bg</col>
-        <col id="1" translatable="yes">Bulgarian</col>
-      </row>
-      <row>
-        <col id="0">ca</col>
-        <col id="1" translatable="yes">Catalan</col>
-      </row>
-      <row>
-        <col id="0">ceb</col>
-        <col id="1" translatable="yes">Cebuano</col>
-      </row>
-      <row>
-        <col id="0">zh-CN</col>
-        <col id="1" translatable="yes">Chinese (Simplified)</col>
-      </row>
-      <row>
-        <col id="0">zh-TW</col>
-        <col id="1" translatable="yes">Chinese (Traditional)</col>
-      </row>
-      <row>
-        <col id="0">co</col>
-        <col id="1" translatable="yes">Corsican</col>
-      </row>
-      <row>
-        <col id="0">hr</col>
-        <col id="1" translatable="yes">Croatian</col>
-      </row>
-      <row>
-        <col id="0">cs</col>
-        <col id="1" translatable="yes">Czech</col>
-      </row>
-      <row>
-        <col id="0">da</col>
-        <col id="1" translatable="yes">Danish</col>
-      </row>
-      <row>
-        <col id="0">nl</col>
-        <col id="1" translatable="yes">Dutch</col>
-      </row>
-      <row>
-        <col id="0">en</col>
-        <col id="1" translatable="yes">English</col>
-      </row>
-      <row>
-        <col id="0">eo</col>
-        <col id="1" translatable="yes">Esperanto</col>
-      </row>
-      <row>
-        <col id="0">et</col>
-        <col id="1" translatable="yes">Estonian</col>
-      </row>
-      <row>
-        <col id="0">fi</col>
-        <col id="1" translatable="yes">Finnish</col>
-      </row>
-      <row>
-        <col id="0">fr</col>
-        <col id="1" translatable="yes">French</col>
-      </row>
-      <row>
-        <col id="0">fy</col>
-        <col id="1" translatable="yes">Frisian</col>
-      </row>
-      <row>
-        <col id="0">gl</col>
-        <col id="1" translatable="yes">Galician</col>
-      </row>
-      <row>
-        <col id="0">ka</col>
-        <col id="1" translatable="yes">Georgian</col>
-      </row>
-      <row>
-        <col id="0">de</col>
-        <col id="1" translatable="yes">German</col>
-      </row>
-      <row>
-        <col id="0">el</col>
-        <col id="1" translatable="yes">Greek</col>
-      </row>
-      <row>
-        <col id="0">gu</col>
-        <col id="1" translatable="yes">Gujarati</col>
-      </row>
-      <row>
-        <col id="0">ht</col>
-        <col id="1" translatable="yes">Haitian Creole</col>
-      </row>
-      <row>
-        <col id="0">ha</col>
-        <col id="1" translatable="yes">Hausa</col>
-      </row>
-      <row>
-        <col id="0">haw</col>
-        <col id="1" translatable="yes">Hawaiian</col>
-      </row>
-      <row>
-        <col id="0">he</col>
-        <col id="1" translatable="yes">Hebrew</col>
-      </row>
-      <row>
-        <col id="0">hi</col>
-        <col id="1" translatable="yes">Hindi</col>
-      </row>
-      <row>
-        <col id="0">hmn</col>
-        <col id="1" translatable="yes">Hmong</col>
-      </row>
-      <row>
-        <col id="0">hu</col>
-        <col id="1" translatable="yes">Hungarian</col>
-      </row>
-      <row>
-        <col id="0">is</col>
-        <col id="1" translatable="yes">Icelandic</col>
-      </row>
-      <row>
-        <col id="0">ig</col>
-        <col id="1" translatable="yes">Igbo</col>
-      </row>
-      <row>
-        <col id="0">id</col>
-        <col id="1" translatable="yes">Indonesian</col>
-      </row>
-      <row>
-        <col id="0">ga</col>
-        <col id="1" translatable="yes">Irish</col>
-      </row>
-      <row>
-        <col id="0">it</col>
-        <col id="1" translatable="yes">Italian</col>
-      </row>
-      <row>
-        <col id="0">ja</col>
-        <col id="1" translatable="yes">Japanese</col>
-      </row>
-      <row>
-        <col id="0">jw</col>
-        <col id="1" translatable="yes">Javanese</col>
-      </row>
-      <row>
-        <col id="0">kn</col>
-        <col id="1" translatable="yes">Kannada</col>
-      </row>
-      <row>
-        <col id="0">kk</col>
-        <col id="1" translatable="yes">Kazakh</col>
-      </row>
-      <row>
-        <col id="0">km</col>
-        <col id="1" translatable="yes">Khmer</col>
-      </row>
-      <row>
-        <col id="0">ko</col>
-        <col id="1" translatable="yes">Korean</col>
-      </row>
-      <row>
-        <col id="0">ku</col>
-        <col id="1" translatable="yes">Kurdish</col>
-      </row>
-      <row>
-        <col id="0">ky</col>
-        <col id="1" translatable="yes">Kyrgyz</col>
-      </row>
-      <row>
-        <col id="0">lo</col>
-        <col id="1" translatable="yes">Lao</col>
-      </row>
-      <row>
-        <col id="0">la</col>
-        <col id="1" translatable="yes">Latin</col>
-      </row>
-      <row>
-        <col id="0">lv</col>
-        <col id="1" translatable="yes">Latvian</col>
-      </row>
-      <row>
-        <col id="0">lt</col>
-        <col id="1" translatable="yes">Lithuanian</col>
-      </row>
-      <row>
-        <col id="0">lb</col>
-        <col id="1" translatable="yes">Luxembourgish</col>
-      </row>
-      <row>
-        <col id="0">mk</col>
-        <col id="1" translatable="yes">Macedonian</col>
-      </row>
-      <row>
-        <col id="0">mg</col>
-        <col id="1" translatable="yes">Malagasy</col>
-      </row>
-      <row>
-        <col id="0">ms</col>
-        <col id="1" translatable="yes">Malay</col>
-      </row>
-      <row>
-        <col id="0">ml</col>
-        <col id="1" translatable="yes">Malayalam</col>
-      </row>
-      <row>
-        <col id="0">mt</col>
-        <col id="1" translatable="yes">Maltese</col>
-      </row>
-      <row>
-        <col id="0">mi</col>
-        <col id="1" translatable="yes">Maori</col>
-      </row>
-      <row>
-        <col id="0">mr</col>
-        <col id="1" translatable="yes">Marathi</col>
-      </row>
-      <row>
-        <col id="0">mn</col>
-        <col id="1" translatable="yes">Mongolian</col>
-      </row>
-      <row>
-        <col id="0">my</col>
-        <col id="1" translatable="yes">Myanmar (Burmese)</col>
-      </row>
-      <row>
-        <col id="0">ne</col>
-        <col id="1" translatable="yes">Nepali</col>
-      </row>
-      <row>
-        <col id="0">no</col>
-        <col id="1" translatable="yes">Norwegian</col>
-      </row>
-      <row>
-        <col id="0">ny</col>
-        <col id="1" translatable="yes">Nyanja (Chichewa)</col>
-      </row>
-      <row>
-        <col id="0">ps</col>
-        <col id="1" translatable="yes">Pashto</col>
-      </row>
-      <row>
-        <col id="0">fa</col>
-        <col id="1" translatable="yes">Persian</col>
-      </row>
-      <row>
-        <col id="0">pl</col>
-        <col id="1" translatable="yes">Polish</col>
-      </row>
-      <row>
-        <col id="0">pt</col>
-        <col id="1" translatable="yes">Portuguese (Portugal, Brazil)</col>
-      </row>
-      <row>
-        <col id="0">pa</col>
-        <col id="1" translatable="yes">Punjabi</col>
-      </row>
-      <row>
-        <col id="0">ro</col>
-        <col id="1" translatable="yes">Romanian</col>
-      </row>
-      <row>
-        <col id="0">ru</col>
-        <col id="1" translatable="yes">Russian</col>
-      </row>
-      <row>
-        <col id="0">sm</col>
-        <col id="1" translatable="yes">Samoan</col>
-      </row>
-      <row>
-        <col id="0">gd</col>
-        <col id="1" translatable="yes">Scots Gaelic</col>
-      </row>
-      <row>
-        <col id="0">sr</col>
-        <col id="1" translatable="yes">Serbian</col>
-      </row>
-      <row>
-        <col id="0">st</col>
-        <col id="1" translatable="yes">Sesotho</col>
-      </row>
-      <row>
-        <col id="0">sn</col>
-        <col id="1" translatable="yes">Shona</col>
-      </row>
-      <row>
-        <col id="0">sd</col>
-        <col id="1" translatable="yes">Sindhi</col>
-      </row>
-      <row>
-        <col id="0">si</col>
-        <col id="1" translatable="yes">Sinhala (Sinhalese)</col>
-      </row>
-      <row>
-        <col id="0">sk</col>
-        <col id="1" translatable="yes">Slovak</col>
-      </row>
-      <row>
-        <col id="0">sl</col>
-        <col id="1" translatable="yes">Slovenian</col>
-      </row>
-      <row>
-        <col id="0">so</col>
-        <col id="1" translatable="yes">Somali</col>
-      </row>
-      <row>
-        <col id="0">es</col>
-        <col id="1" translatable="yes">Spanish</col>
-      </row>
-      <row>
-        <col id="0">su</col>
-        <col id="1" translatable="yes">Sundanese</col>
-      </row>
-      <row>
-        <col id="0">sw</col>
-        <col id="1" translatable="yes">Swahili</col>
-      </row>
-      <row>
-        <col id="0">sv</col>
-        <col id="1" translatable="yes">Swedish</col>
-      </row>
-      <row>
-        <col id="0">tl</col>
-        <col id="1" translatable="yes">Tagalog (Filipino)</col>
-      </row>
-      <row>
-        <col id="0">tg</col>
-        <col id="1" translatable="yes">Tajik</col>
-      </row>
-      <row>
-        <col id="0">ta</col>
-        <col id="1" translatable="yes">Tamil</col>
-      </row>
-      <row>
-        <col id="0">te</col>
-        <col id="1" translatable="yes">Telugu</col>
-      </row>
-      <row>
-        <col id="0">th</col>
-        <col id="1" translatable="yes">Thai</col>
-      </row>
-      <row>
-        <col id="0">tr</col>
-        <col id="1" translatable="yes">Turkish</col>
-      </row>
-      <row>
-        <col id="0">uk</col>
-        <col id="1" translatable="yes">Ukrainian</col>
-      </row>
-      <row>
-        <col id="0">ur</col>
-        <col id="1" translatable="yes">Urdu</col>
-      </row>
-      <row>
-        <col id="0">uz</col>
-        <col id="1" translatable="yes">Uzbek</col>
-      </row>
-      <row>
-        <col id="0">vi</col>
-        <col id="1" translatable="yes">Vietnamese</col>
-      </row>
-      <row>
-        <col id="0">cy</col>
-        <col id="1" translatable="yes">Welsh</col>
-      </row>
-      <row>
-        <col id="0">xh</col>
-        <col id="1" translatable="yes">Xhosa</col>
-      </row>
-      <row>
-        <col id="0">yi</col>
-        <col id="1" translatable="yes">Yiddish</col>
-      </row>
-      <row>
-        <col id="0">yo</col>
-        <col id="1" translatable="yes">Yoruba</col>
-      </row>
-      <row>
-        <col id="0">zu</col>
-        <col id="1" translatable="yes">Zulu</col>
-      </row>
-    </data>
   </object>
 </interface>

src/utils.js
@@ -41,6 +41,7 @@ var debounce = (f, wait, immediate) => {
 const { Gtk, Gio, GLib, GObject, Gdk, GdkPixbuf } = imports.gi
 const ByteArray = imports.byteArray
 const ngettext = imports.gettext.ngettext
+const { iso_639_2_path, iso_3166_1_path } = imports.isoCodes
 
 let verbose = false
 var setVerbose = value => verbose = value
@@ -74,6 +75,17 @@ var markupEscape = text => text ? GLib.markup_escape_text(text, -1) : ''
 
 var regexEscape = str => str ? str.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&') : ''
 
+var readJSON = file => {
+    try {
+        const [success, data, /*tag*/] = file.load_contents(null)
+        if (success) return JSON.parse(data instanceof Uint8Array
+            ? ByteArray.toString(data) : data.toString())
+        else throw new Error()
+    } catch (e) {
+        return {}
+    }
+}
+
 var glibcLocaleToJsLocale = x => x === 'C' ? 'en' : x.split('.')[0].replace('_', '-')
 var locales = GLib.get_language_names().map(glibcLocaleToJsLocale)
 try {
@@ -81,6 +93,65 @@ try {
     const locale = glibcLocaleToJsLocale(settings.get_string('region'))
     if (locale) locales = locale
 } catch (e) {}
+
+var languageNames = new Map()
+var alpha_3_to_alpha_2 = new Map()
+var regionNames = new Map()
+const iso_639_2 = readJSON(Gio.File.new_for_path(iso_639_2_path))
+const iso_3166_1 = readJSON(Gio.File.new_for_path(iso_3166_1_path))
+const hasIso_639_2 = '639-2' in iso_639_2
+const hasIso_3166_1 = '3166-1' in iso_3166_1
+if (hasIso_639_2) for (const obj of iso_639_2['639-2']) {
+    if (!obj) continue
+    const { alpha_2, alpha_3, name } = obj
+    if (alpha_2) languageNames.set(alpha_2, name)
+    if (alpha_3) languageNames.set(alpha_3, name)
+    if (alpha_2 && alpha_3) alpha_3_to_alpha_2.set(alpha_3, alpha_2)
+}
+if (hasIso_3166_1) for (const obj of iso_3166_1['3166-1']) {
+    if (!obj) continue
+    const { alpha_2, alpha_3, name, common_name } = obj
+    const n = common_name || name
+    if (alpha_2) regionNames.set(alpha_2, n)
+    if (alpha_3) regionNames.set(alpha_3, n)
+}
+var isRegionCode = str => str === str.toUpperCase()
+// get langauge names
+// should perhaps use `Intl.DisplayNames()` instead once it becomes available
+var getLanguageDisplayName = code => {
+    try {
+        code = Intl.getCanonicalLocales(code)[0]
+    } catch (e) {
+        return code
+    }
+    if (!hasIso_639_2) return code
+    const [language, ...rest] = code.split('-')
+    const languageName = languageNames.get(language)
+    const languageDisplayName = languageName
+        ? GLib.dgettext('iso_639-2', languageName)
+        : language
+
+    const region = rest.find(isRegionCode)
+    const regionName = region ? regionNames.get(region) : null
+    const regionDisplayName = regionName
+        ? GLib.dgettext('iso_3166-1', regionName)
+        : region
+
+    return regionDisplayName
+        ? _('%s (%s)').format(languageDisplayName, regionDisplayName)
+        : languageDisplayName
+}
+// convert alpha-3 to alpha-2 if possible
+var getAlpha2 = code => {
+    try {
+        code = Intl.getCanonicalLocales(code)[0]
+    } catch (e) {
+        return
+    }
+    const lang = code.split('-')[0]
+    if (lang.length === 2) return lang
+    return alpha_3_to_alpha_2.get(lang) || lang
+}
 
 var formatMinutes = n => {
     n = Math.round(n)
@@ -182,17 +253,6 @@ var recursivelyDeleteDir = dir => {
         else if (type == Gio.FileType.DIRECTORY) recursivelyDeleteDir(child)
     }
     dir.delete(null)
-}
-
-var readJSON = file => {
-    try {
-        const [success, data, /*tag*/] = file.load_contents(null)
-        if (success) return JSON.parse(data instanceof Uint8Array
-            ? ByteArray.toString(data) : data.toString())
-        else throw new Error()
-    } catch (e) {
-        return {}
-    }
 }
 
 var Storage = GObject.registerClass({

commit 2a4e81219f4fb6d23a85aaa2e4cf4412dbca6a33

Fix Wiktionary internal links
The bug is regression caused by 72d5c70; The language name
"English" gets turned into "english" by `Intl.getCanonicalLocales`
in the `getAlpha2` function.

Fixes #577
Closes #535

src/web/lookup.js
@@ -20,7 +20,7 @@ const wiktionary = (word, language = 'en') => {
         .then(json => {
             const results = language.length === 2
                 ? json[language]
-                : Object.values(json).find(x => x[0].language === language)
+                : Object.values(json).find(x => x[0].language.toLowerCase() === language)
             results.forEach(el => {
                 el.definitions.forEach(x => {
                     x.definition = toPangoMarkup(x.definition, baseURL)