a52d0f3e9f1050a3edd529317adb15f1cbcec652
John Factotum <50942278+johnfactotum@users.noreply.github.com>
2023-10-14 21:29:54 +0800

    Fix default target language for translation tool
    See a22a9861ca273576dab3a5bf7191f3d36f71ef5a

--- src/selection-tools.js
+++ src/selection-tools.js
@@ -25,7 +25,7 @@ const getGoogleTranslateLanguages = utils.memoize(() => {
     const displayName = new Intl.DisplayNames(locales, { type: 'language' })
     const langs = ['af', 'sq', 'am', 'ar', 'hy', 'as', 'ay', 'az', 'bm', 'eu', 'be', 'bn', 'bho', 'bs', 'bg', 'ca', 'ceb', 'zh-CN', 'zh-TW', 'co', 'hr', 'cs', 'da', 'dv', 'doi', 'nl', 'en', 'eo', 'et', 'ee', 'fil', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gn', 'gu', 'ht', 'ha', 'haw', 'he', 'hi', 'hmn', 'hu', 'is', 'ig', 'ilo', 'id', 'ga', 'it', 'ja', 'jv', 'kn', 'kk', 'km', 'rw', 'gom', 'ko', 'kri', 'ku', 'ckb', 'ky', 'lo', 'la', 'lv', 'ln', 'lt', 'lg', 'lb', 'mk', 'mai', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mni-Mtei', 'lus', 'mn', 'my', 'ne', 'no', 'ny', 'or', 'om', 'ps', 'fa', 'pl', 'pt', 'pa', 'qu', 'ro', 'ru', 'sm', 'sa', 'gd', 'nso', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tl', 'tg', 'ta', 'tt', 'te', 'th', 'ti', 'ts', 'tr', 'tk', 'ak', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu']
     let defaultLang
-    for (const locale of locales) {
+    for (const locale of locales.map(locale => locale.baseName)) {
         if (locale === 'zh-CN' || locale === 'zh-TW') {
             defaultLang = locale
             break
