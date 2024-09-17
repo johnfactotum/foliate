4fbeecbc762fe5cda050aae7bb7e51fec3f91fe0
John Factotum <50942278+johnfactotum@users.noreply.github.com>
2024-09-18 06:54:15 +0800

    OPDS: resolve bare search template URL in feed
    Fixes #1401

--- src/opds/main.js
+++ src/opds/main.js
@@ -564,8 +564,8 @@ const renderFeed = async (feed, baseURL) => {
             else if (atomSearch) renderSearch({
                 metadata: { title: atomSearch.title },
                 // NOTE: no full OpenSearch support here
-                search: map => atomSearch.href.replaceAll('{searchTerms}',
-                    encodeURIComponent(map.get(null).get('searchTerms'))),
+                search: map => resolveURL(atomSearch.href.replaceAll('{searchTerms}',
+                    encodeURIComponent(map.get(null).get('searchTerms'))), baseURL),
                 params: [{ name: 'searchTerms' }],
             })
         }
